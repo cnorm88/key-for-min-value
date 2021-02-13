1f4b2dbeed14e7dbc5adc47ecd113177a6c0f44b
cnorm88 <cnorm88@gmail.com>
2021-02-13 17:42:36 -0500

    commit tree 91e5966bf7d317c7cf09c8c5ecb64cc291288e3d

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,16 @@
 
 def key_for_min_value(name_hash)
 
+	min = nil
+	value_min = nil
+	name_hash.each do |key, value|
+		if min == nil
+			min = key
+			value_min = value
+		elsif value < value_min
+			min = key
+			value_min = value  
+		end 
+	end
+	min
 end
